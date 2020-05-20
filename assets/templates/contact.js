d15db3f9630f1d28cd3aa93b76200ce29296ea77
Маша Юрлова <mariya.yurlova@gmail.com>
2020-05-20 10:27:01 +0300

    Update

---
+++ assets/templates/contact.js
@@ -0,0 +1,10 @@
+import './../common/_common.scss';
+
+/**
+ * Add Mastak Library
+ */
+
+import './../components/header/header';
+import './../components/footer/footer';
+import './../components/contact/contact';
+import './../components/popup/popup';
---
+++ assets/templates/delivery.js
@@ -0,0 +1,10 @@
+import './../common/_common.scss';
+
+/**
+ * Add Mastak Library
+ */
+
+import './../components/header/header';
+import './../components/footer/footer';
+import './../components/delivery/delivery';
+import './../components/popup/popup';
