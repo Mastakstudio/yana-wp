d75ffdac9385630645750459cd34d38430a11871
Маша Юрлова <mariya.yurlova@gmail.com>
2020-06-23 11:51:48 +0300

    Update

--- assets/components/footer/footer.js
+++ assets/components/footer/footer.js
@@ -1,2 +1,8 @@
 import './footer.scss';
 import $ from 'jquery';
+import 'remodal'
+
+
+$(document).on('opening', '.footer__remodal', function () {
+    $('.footer__remodal').parent().addClass('footer__remodal-wrapper');
+});
