7cc40c4df12c1ebdf77ca432d5f45690287fd005
Androsov Aleksandr <gilgust090891@gmail.com>
2020-06-29 18:24:38 +0300

    Update popup.js

--- assets/components/popup/popup.js
+++ assets/components/popup/popup.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 import 'remodal'
 
 $(document).on('closing', '.remodal', function (e) {
-    $('.youtube-video').each(function(index) {
+    $(this).find('.youtube-video').each(function(index) {
         $(this).attr('src', $(this).attr('src'));
         return false;
     });
