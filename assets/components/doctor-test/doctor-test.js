c25ee74be607ba01e8397d53e6408527a1c96a73
Маша Юрлова <mariya.yurlova@gmail.com>
2021-05-05 10:00:46 +0300

    update

--- assets/components/doctor-test/doctor-test.js
+++ assets/components/doctor-test/doctor-test.js
@@ -1,13 +1,12 @@
 import './doctor-test.scss';
 import $ from 'jquery';
 
-let offsetTop = $('#ID1').offset().top;
-let offsetWrap = $('.doctor-test__wrap').offset().top;
+let offsetTop = $('.doctor-test__wrap').offset().top;
+
 $(".doctor-test__pattern1").css("margin-top",offsetTop);
 $(".doctor-test__pattern2").css("margin-top",offsetTop);
-let razn = offsetWrap - offsetTop;
 let height = parseInt($(".doctor-test__wrap").height());
 
-$(".doctor-test__pattern1").css("height", height-razn);
-$(".doctor-test__pattern2").css("height", height-razn);
+$(".doctor-test__pattern1").css("height", height);
+$(".doctor-test__pattern2").css("height", height);
 
